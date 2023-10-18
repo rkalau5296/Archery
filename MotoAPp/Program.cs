0136a201455f2cd79248e14c54d23c2855680e7d
rafal <rafal.kalata.itservice@gmail.com>
2023-10-18 23:06:44 +0200

    add classes

---
+++ MotoAPp/Program.cs
@@ -0,0 +1,31 @@
+﻿using MotoAPp.Data;
+using MotoAPp.Entities;
+using MotoAPp.Repositories;
+
+var employeeRepository = new SqlRepoitory<Employee>(new MotoAppDbContext());
+AddEmployees(employeeRepository);
+AddManagers(employeeRepository);
+WriteAllToConsole(employeeRepository);
+
+static void AddEmployees(IRepository<Employee> employeeRepository)
+{
+    employeeRepository.Add(new Employee { FirstName = "Adam" });
+    employeeRepository.Add(new Employee { FirstName = "Piotr" });
+    employeeRepository.Add(new Employee { FirstName = "Zuza" });
+    employeeRepository.Save();
+}
+
+static void AddManagers(IWriteRepository<Manager> managerRepository)
+{
+    managerRepository.Add(new Manager { FirstName = "Przemek" });
+    managerRepository.Add(new Manager { FirstName = "Tomek" });    
+    managerRepository.Save();
+}
+static void WriteAllToConsole(IReadRepository<IEntity> repository)
+{
+    var items = repository.GetAll();
+    foreach(var item in items)
+    {
+        Console.WriteLine(item);
+    }
+}
